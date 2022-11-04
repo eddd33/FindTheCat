a03f6fe003f80faf599bf1d15fad60fdd4e22c9f
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-11-04 18:57:05 +0100

    ajout de commentaires

--- src/ftc.c
+++ src/ftc.c
@@ -8,30 +8,34 @@
 
 void listdir(const char *name, int indent)
 {
-    DIR *dirp;
-    struct dirent *dp;
+    DIR *dirp; // pointeur de répertoire
+    struct dirent *dp;  // pointeur de fichier
 
-    if (!(dirp = opendir(name)))
+    if (!(dirp = opendir(name))) //ouvre le repertoire
         return;
 
-    while ((dp = readdir(dirp)) != NULL)
+    while ((dp = readdir(dirp)) != NULL) //tant qu'il y a des fichiers
     {
-        if (dp->d_type == DT_DIR)
+        if (dp->d_type == DT_DIR) //si c'est un répertoire (DT_DIR est le type répertoire)
         {
-            char path[1024];
-            if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+            char path[1024]; //création d'un chemin
+            if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) //si c'est le répertoire courant ou le répertoire parent
                 continue;
-            snprintf(path, sizeof(path), "%s/%s", name, dp->d_name);
-            printf("%*s[%s]\n", indent, "", dp->d_name);
-            listdir(path, indent + 2);
+            snprintf(path, sizeof(path), "%s/%s", name, dp->d_name); //on concatène le chemin du répertoire courant avec le nom du répertoire
+            printf("%*s[%s]\n", indent, "", dp->d_name); //on affiche le nom du répertoire
+            listdir(path, indent + 2); //on appelle la fonction récursivement
         }
         else
         {
-            printf("%*s- %s\n", indent, "", dp->d_name);
+            printf("%*s- %s\n", indent, "", dp->d_name); //on affiche le nom du fichier
         }
     }
-    closedir(dirp);
+    closedir(dirp); //on ferme le répertoire
 }
+
+//q3
+//void size ()
+
 int main(int argc, char *argv[])
 {
 
@@ -43,6 +47,11 @@ int main(int argc, char *argv[])
             printf("La valeur du flag %s est %s.\n", argv[i + 1], argv[i + 2]);
             break;
         };
+        //if (strcmp(argv[i], "-size") == 0)
+        //{
+            //size(argv[i + 1]);
+          //  break;
+        //};"))
     }
     listdir(".",0);
 
