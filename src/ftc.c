3e0bd0ad18244707a7a1c96b3b418507740e5e2c
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-11-05 11:10:40 +0100

    avancée sur -size

--- src/ftc.c
+++ src/ftc.c
@@ -6,7 +6,88 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-void listdir(const char *name, int indent)
+FLAG_SIZE = 0;
+//q3
+int taille (char *nom)
+{
+    FILE *fichier = NULL; //pointeur sur le fichier
+    fichier = fopen(nom, "r"); //ouverture du fichier en lecture
+    int size = 0;
+    if (fichier != NULL)
+    {
+        fseek(fichier, 0, SEEK_END); // on lit le fichier jusqu'à la fin
+        size = ftell(fichier); // on récupère la position du curseur
+        //printf("Taille du fichier : %d octets");
+        return size;
+        fclose(fichier);
+    }
+}
+
+bool comparaison (char *nom, char *fichier){
+    char signe = nom[0];
+    char unité = nom[-1];
+    char *tamp;
+    //for (size_t i = 1; i < strlen(nom)-1; i++)
+    //{
+       // tamp[i-1] = nom[i];
+    //}
+    
+    int target = atoi(tamp);
+    if (signe == "+"){
+        if (unité == "c"){
+            if (target < taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "k"){
+            target = target * 1024;
+            if (target < taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "M"){
+            target = target * 1024 * 1024;
+            if (target < taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "G"){
+            target = target * 1024 * 1024 * 1024;
+            if (target < taille(fichier)){
+                return true;
+            }
+        }
+        return false;
+    }
+    if (signe == "-"){
+        if (unité == "c"){
+            if (target > taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "k"){
+            target = target * 1024;
+            if (target > taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "M"){
+            target = target * 1024 * 1024;
+            if (target > taille(fichier)){
+                return true;
+            }
+        }
+        if (unité == "G"){
+            target = target * 1024 * 1024 * 1024;
+            if (target > taille(fichier)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+void listdir(const char *name, int indent,char *val)
 {
     DIR *dirp; // pointeur de répertoire
     struct dirent *dp;  // pointeur de fichier
@@ -23,23 +104,31 @@ void listdir(const char *name, int indent)
                 continue;
             snprintf(path, sizeof(path), "%s/%s", name, dp->d_name); //on concatène le chemin du répertoire courant avec le nom du répertoire
             printf("%*s[%s]\n", indent, "", dp->d_name); //on affiche le nom du répertoire
-            listdir(path, indent + 2); //on appelle la fonction récursivement
+            listdir(path, indent + 2,val); //on appelle la fonction récursivement
         }
-        else
+        else //si c'est un fichier
         {
+            if (FLAG_SIZE == 1)
+            {
+                if (comparaison(val,dp->d_name) == true)
+                {
+                    printf("%*s- %s\n", indent, "", dp->d_name); //on affiche le nom du fichier
+                }
             printf("%*s- %s\n", indent, "", dp->d_name); //on affiche le nom du fichier
+            taille(dp->d_name);
+            }
         }
-    }
     closedir(dirp); //on ferme le répertoire
+    }
 }
 
-//q3
-//void size ()
+
 
 int main(int argc, char *argv[])
 {
 
     int size = strlen(*argv);
+    char *val;
     for (int i = 0; i < size; i++)
     {
         if (strcmp(argv[i], "-test") == 0)
@@ -47,13 +136,14 @@ int main(int argc, char *argv[])
             printf("La valeur du flag %s est %s.\n", argv[i + 1], argv[i + 2]);
             break;
         };
-        //if (strcmp(argv[i], "-size") == 0)
-        //{
-            //size(argv[i + 1]);
-          //  break;
-        //};"))
+        if (strcmp(argv[i], "-size") == 0)
+        {
+            FLAG_SIZE = 1;
+            val = argv[i + 1];
+            break;
+        };
     }
-    listdir(".",0);
+    listdir(".",0,val);
 
     return 0;
 }
